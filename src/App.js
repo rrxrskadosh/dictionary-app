f706962c1173341fea87a509c148c318be9f602b
rrxrskadosh <kadoshrr2019@gmail.com>
2022-08-08 23:42:11 -0500

    Building Structure Search Input and Button, Boilerplate React

--- src/App.js
+++ src/App.js
@@ -36,10 +36,24 @@ const App = () => {
           <div className='col-12 text-center fw-bold fs-1 p-3 bg-success text-white'>
             Pocket Dictionary
           </div>
+          <div className='form-floating bg-primary py-3 pb-5 d-flex justify-content-center'>
+            <input
+              type="text"
+              className="form-control-sm border-0 px-2 col-md-3 col-sm-4"
+              placeholder="Type your word"
+              id="floatingInput"
+              />
+              <button className="btn btn-dark text-ligth col-md-1 col-sm-2 mx-2">
+                Search
+              </button>
+            <div className="fs-1 text-capitalize text-center fw-bold text-decoration-underline text-white bg-dark extra">
+              You can search in the BOX!
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
-}
+};
 
 export default App;
